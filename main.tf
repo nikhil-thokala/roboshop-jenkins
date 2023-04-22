89d18f0af187d204c530c6ae362ecfc136027915
nikhilt <nikhil.t1226@gmail.com>
2023-04-22 17:37:14 +0100

    jenkins

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "jenkins_job" "job" {
   name     = "roboshop"
   folder   = jenkins_folder.folders.id
   template = templatefile("${path.module}/sb-job.xml", {
-    description = "An example job created from Terraform"
+    description = ""
   })
 }
 
