049e3fdb1de7c7ab06031f56358d7210c131549c
nikhilt <nikhil.t1226@gmail.com>
2023-04-23 12:30:38 +0100

    jenkins

--- vars.tf
+++ vars.tf
@@ -1,10 +1,16 @@
 variable "folders" {
-  default = ["infra"]
+  default = ["infra", "CI-Pipeline"]
 }
 
  variable "jobs" {
    default = [
-     { name = "roboshop", folder = "infra", repo_url = "http://github.com/roboshop-infra"}
+     { name = "roboshop", folder = "infra", repo_url = "http://github.com/roboshop-infra"},
+     { name = "frontend", folder = "CI-Pipeline", repo_url = "http://github.com/roboshop-infra"},
+     { name = "cart", folder = "CI-Pipeline", repo_url = "http://github.com/roboshop-infra"},
+     { name = "catalogue", folder = "CI-Pipeline", repo_url = "http://github.com/roboshop-infra"},
+     { name = "user", folder = "CI-Pipeline", repo_url = "http://github.com/roboshop-infra"},
+     { name = "shipping", folder = "CI-Pipeline", repo_url = "http://github.com/roboshop-infra"},
+     { name = "payment", folder = "CI-Pipeline", repo_url = "http://github.com/roboshop-infra"}
 ]
  }
 
